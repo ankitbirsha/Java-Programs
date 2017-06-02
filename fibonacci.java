11553cbaaf3deb8bb52a085caa2098ae0287976a
Ankit Anand <ankit.4anand2309@gmail.com>
2017-06-03 00:37:38 +0530

    added successfully

---
+++ fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class fibonacci
+{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number upto which you want fibonacci series");
+		int n=sc.nextInt();
+		int first=0;
+		int second=1;
+		System.out.println(first);
+		System.out.println(second);
+
+		for(int i=0;i<n-2;i++)
+		{
+			int next=first+second;
+			first=second;
+			second=next;
+			System.out.println(next);
+		}
+	}
+}
